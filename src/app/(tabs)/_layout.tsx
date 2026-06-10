8961fb6d9da68f280899a5244d72bcf9f029fca3
Berbet <juliatrindadeberbet@gmail.com>
2026-06-10 19:34:52 -0300

    :construction: First changes

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,23 +1,47 @@
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Octicons from '@expo/vector-icons/Octicons';
+import Feather from '@expo/vector-icons/Feather';
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: '#007AFF' }}>
+    <Tabs screenOptions={{  tabBarActiveTintColor: '#075E54'}}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} />,
+          title: 'WhatsApp',
+          tabBarIcon: ({ color }) => <MaterialIcons name="chat" size={24} color={color} />,
         }}
       />
       <Tabs.Screen
-        name="profile"
+        name="updates"
         options={{
-          title: 'Perfil',
-          tabBarIcon: ({ color }) => <Ionicons name="person" size={24} color={color} />,
+          title: 'Atualizações',
+          tabBarIcon: ({ color }) => <Octicons name="sync" size={24} color={color} />,
         }}
       />
+
+         <Tabs.Screen
+        name="communities"
+        options={{
+          title: 'Comunidades',
+          tabBarIcon: ({ color }) => <MaterialIcons name="groups" size={24} color={color} />,
+        }}
+      />
+
+
+<Tabs.Screen
+        name="calls"
+        options={{
+          title: 'Ligações',
+          tabBarIcon: ({ color }) => <Feather name="phone" size={24} color={color} />,
+        }}
+      />
+
     </Tabs>
-  );
+
+  )
 }
+    
+   
