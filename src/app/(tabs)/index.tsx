af3508a32d9c3610a34ad62fd012a1b9cb7f7916
Berbet19 <Aluno@L3T09.acad.etecpoa>
2026-06-15 09:44:34 -0300

    :construction: Some changes in the size of the tabs

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity} from 'react-native';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
 
 function MeuBotao({ buttontext }: { buttontext: string }) {
   return (
@@ -23,7 +24,7 @@ export default function HomeScreen() {
         <MeuBotao buttontext="Não Lidas" />
         <MeuBotao buttontext="Favoritos" />
         <MeuBotao buttontext="Grupos" />
-         <MeuBotao buttontext="+" />
+        <MeuBotao buttontext="+" />
 
 </ScrollView>
        
@@ -31,36 +32,35 @@ export default function HomeScreen() {
       
      <View style={styles.Rowcontainer}>
       <Image style={styles.img} source={require ('../../../assets/images/Contacts.png')}/>
-      <Text style={styles.title}>Meu Projeto Começa Aqui!</Text>
+      <Text style={styles.title}>Contato 1 </Text>
       </View>
 
-<Text style={styles.subtitle}>Meu Projeto Começa Aqui!</Text>
+      <Text style={styles.subtitle}><Ionicons name="checkmark-done-outline" size={15} color="grey" />Estou saindo!</Text>
      
-      
-
+    
 <View style={styles.Rowcontainer}>
       <Image style={styles.img} source={require ('../../../assets/images/Contacts.png')}/>
-      <Text style={styles.title}>Meu Projeto Começa Aqui!</Text>
+      <Text style={styles.title}>Contato 2 </Text>
       </View>
-      <Text style={styles.subtitle}>Meu Projeto Começa Aqui!</Text>
+      <Text style={styles.subtitle}><Ionicons name="checkmark-done-outline" size={15} color="grey" />HELLO</Text>
 
       <View style={styles.Rowcontainer}>
       <Image style={styles.img} source={require ('../../../assets/images/Contacts.png')}/>
-      <Text style={styles.title}>Meu Projeto Começa Aqui!</Text>
+      <Text style={styles.title}>Contato 3</Text>
       </View>
-      <Text style={styles.subtitle}>Meu Projeto Começa Aqui!</Text>
+      <Text style={styles.subtitle}><Ionicons name="checkmark-done-outline" size={15} color="grey" /> </Text>
 
       <View style={styles.Rowcontainer}>
       <Image style={styles.img} source={require ('../../../assets/images/Contacts.png')}/>
-      <Text style={styles.title}>Meu Projeto Começa Aqui!</Text>
+      <Text style={styles.title}>Contato 4</Text>
       </View>
-      <Text style={styles.subtitle}>Meu Projeto Começa Aqui!</Text>
+      <Text style={styles.subtitle}><Ionicons name="checkmark-done-outline" size={15} color="grey" /></Text>
 
       <View style={styles.Rowcontainer}>
       <Image style={styles.img} source={require ('../../../assets/images/Contacts.png')}/>
-      <Text style={styles.title}>Meu Projeto Começa Aqui!</Text>
+      <Text style={styles.title}>Contato 5</Text>
       </View>
-      <Text style={styles.subtitle}>Meu Projeto Começa Aqui!</Text>
+      <Text style={styles.subtitle}></Text>
       
 
     </View>
@@ -91,7 +91,7 @@ const styles = StyleSheet.create({
  Rowcontainer :{
   flexDirection: 'row',
  alignItems : 'center',
- gap: 5,
+ gap: 10,
 
 
 },
@@ -105,6 +105,9 @@ const styles = StyleSheet.create({
     marginHorizontal: 5,
     height: 30,
     justifyContent: 'center',
+   borderColor:'grey',
+     borderStyle: 'solid',  
+     borderWidth: 0.5,
   },
   textoDoItem: {
     color: '#fff',
@@ -115,6 +118,7 @@ subtitle:{
 fontSize: 10,
 color: 'grey' ,
 flex: 1,
+paddingLeft: 60,
 },
 
 })
