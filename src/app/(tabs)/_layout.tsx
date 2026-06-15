d18433a2b000bd348b245bb8625705d1d71aab21
Berbet19 <Aluno@L3T09.acad.etecpoa>
2026-06-15 09:44:34 -0300

    oi

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,28 +1,32 @@
-import { Tabs} from 'expo-router';
+import EvilIcons from '@expo/vector-icons/EvilIcons';
+import Feather from '@expo/vector-icons/Feather';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import Octicons from '@expo/vector-icons/Octicons';
-import Feather from '@expo/vector-icons/Feather';
-import { StyleSheet } from 'react-native';
-import {SafeAreaView,} from 'react-native-safe-area-context';
-import {Text, View, TextInput} from 'react-native';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { Tabs } from 'expo-router';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { SafeAreaView, } from 'react-native-safe-area-context';
+
 
 
 
 
 export default function TabsLayout() {
   return (
+   
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: '#075E54',
         tabBarInactiveTintColor: '#888',
         tabBarStyle: {
           backgroundColor: '#000',
+            height: hp('10%'),
+               borderTopWidth: 0, 
          
         },
         headerStyle: {
           backgroundColor: '#075E54',
-          height: hp('9%'),
+          height: hp('12%'),
       
         },
         
@@ -30,6 +34,7 @@ export default function TabsLayout() {
       backgroundColor: '#000',
     },
       }}
+    
     >
     
     
@@ -37,13 +42,18 @@ export default function TabsLayout() {
   name="index"
   options={{
     title: 'Conversas',
-  tabBarIcon: ({ color }) =>   <MaterialIcons name="comment" size={24} color={color} />,
+  tabBarIcon: ({ color }) =>   <MaterialIcons name="comment" size={26} color={color} />,
     
     header: () => (
       <SafeAreaView>
       <View style={styles.customHeader}>
-         
+         <View style={styles.row}>
         <Text style={styles.headerTitleText}>WhatsApp</Text>
+        <View style={styles.botoesHeader}>
+        <EvilIcons name="search" size={24} color="#a0c4c0" />
+        <Feather name="more-vertical" size={24} color="#a0c4c0" />
+       </View>
+        </View>
         
         <TextInput
           style={styles.searchbar}
@@ -64,7 +74,7 @@ export default function TabsLayout() {
         name="updates"
         options={{
           title: 'Atualizações',
-          tabBarIcon: ({ color }) => <Octicons name="sync" size={24} color={color} />,
+          tabBarIcon: ({ color }) => <Octicons name="sync" size={26} color={color} />,
         }}
       />
 
@@ -72,7 +82,7 @@ export default function TabsLayout() {
         name="communities"
         options={{
           title: 'Comunidades',
-          tabBarIcon: ({ color }) => <MaterialIcons name="groups" size={24} color={color} />,
+          tabBarIcon: ({ color }) => <MaterialIcons name="groups" size={26} color={color} />,
         }}
       />
 
@@ -81,23 +91,25 @@ export default function TabsLayout() {
         name="calls"
         options={{
           title: 'Ligações',
-          tabBarIcon: ({ color }) => <Feather name="phone" size={24} color={color} />,
+          tabBarIcon: ({ color }) => <Feather name="phone" size={26} color={color} />,
         }}
       />
 
     </Tabs>
+   
 
   )
+  
 }
 const styles = StyleSheet.create({
   customHeader: {
     backgroundColor: '#075E54', // Todo o bloco será verde!
-    
+     height: hp('15%'),
     width: '100%',
     paddingHorizontal: 16,
-    paddingTop: 45,             // Empurra o conteúdo para baixo da Notch (bateria/hora)
-    paddingBottom: 12,          // Espaço na borda de baixo da barra de pesquisa
-    justifyContent: 'space-between', // Separa o Título da Barra verticalmente
+    paddingTop: 30,            
+    paddingBottom: 15,        
+    justifyContent: 'space-between', 
   },
   headerTitleText: {
     color: '#fff',
@@ -107,12 +119,21 @@ const styles = StyleSheet.create({
   searchbar: {
     backgroundColor: '#054c43', 
     borderRadius: 22,           
-    height: 40,
+   height: 50,
     width: '100%',              
     paddingHorizontal: 16,
     color: '#fff',
     fontSize: 16,
   },
+  row:{
+    flexDirection: 'row', 
+    justifyContent: 'space-between',
+
+  },
+  botoesHeader:{
+      flexDirection: 'row', 
+
+  }
 });
 
 
--- src/app/(tabs)/communities.tsx
+++ src/app/(tabs)/communities.tsx
@@ -1,11 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <View>
       <Text style={styles.title}>Conecte-se com outras pessoas com as comunidades</Text>
-      <Text style={styles.subtitle}>AS cominidades  permitem reunir pessoas em grupos de assuntos especificos e receber avisos de admins com facilidade. As comunidades das quais você participa serão exibidas nessa tela.</Text>
+      <Text style={styles.subtitle}>AS comunidades  permitem reunir pessoas em grupos de assuntos especificos e receber avisos de admins com facilidade. As comunidades das quais você participa serão exibidas nessa tela.</Text>
+        <TextInput style={styles.botao} placeholder='Criar comunidade' placeholderTextColor='white'></TextInput>
       </View>
     </View>
   );
@@ -30,5 +31,8 @@ const styles = StyleSheet.create({
      fontSize: 15,
   
        textAlign: 'justify',
-  }
+  },
+  botao:{
+backgroundColor:'#075E54' , 
+  },
 });
